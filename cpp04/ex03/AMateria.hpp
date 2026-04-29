249104fb5a99ce57ffb256a5da4a099cfe07d354
Megamax21 <lhotemaxime@hotmail.com>
2026-04-29 18:45:14 +0200

    Finishing bonus of cpp04

---
+++ cpp04/ex03/AMateria.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ml-hote <ml-hote@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/29 18:24:15 by ml-hote           #+#    #+#             */
+/*   Updated: 2026/04/29 18:24:30 by ml-hote          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <string>
+#include <iostream>
+
+class AMateria
+{
+protected:
+	const std::string	type;
+
+public:
+	AMateria(std::string const & type);
+	std::string const & getType() const; //Returns the materia type
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
+};
+#endif
